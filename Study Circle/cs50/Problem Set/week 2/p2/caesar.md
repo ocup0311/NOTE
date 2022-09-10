998cc697080428259ae9ba439c8c87f0f53d3691
Ocup <ocup0311@gmail.com>
2022-09-10 16:14:24 +0800

    [ADD][CS50] week2 p2 caesar

---
+++ Study Circle/cs50/Problem Set/week 2/p2/caesar.md
@@ -0,0 +1,32 @@
+[Caesar’s algorithm](https://cs50.harvard.edu/x/2021/psets/2/caesar/#caesar)
+
+- 範例
+
+  ```shell
+  $ ./caesar 13
+  plaintext:  HELLO
+  ciphertext: URYYB
+  ```
+
+- 語法問題：
+
+  - 無法在執行後取得長度，再定義 string 長度
+    - 三個可試解法：
+      - Just define a big array which can take all possible strings you may want to put there
+      - Use `realloc` to change the size of the array if you need more space
+      - Use a pointer to a string and then change which string that pointer points to
+
+  ```c
+  string plaintext = get_string("plaintext: ");
+  int x = strlen(plaintext);
+  char ciphertext[x] = "";
+  // Error：不能用變數
+  ```
+
+  - 因為是 string 是 pointer，所以是 by ref
+
+  ```c
+  string plaintext = get_string("plaintext: ");
+  string ciphertext = plaintext;
+  // ciphertext 只有賦予 plaintext 的 ref
+  ```
---
+++ Study Circle/cs50/Problem Set/week 2/p2/caesar_1.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <math.h>
+#include <cs50.h>
+
+// CONST
+int A = 65, Z = 90, a = 97, z = 122;
+
+// main
+int main(int n, string p[])
+{
+    // exception
+    if(n != 2)
+    {
+        printf("Need only one Key!\n");
+        return true;
+    }
+
+    // var
+    int key = atoi(p[1]);
+    printf("key: %i\n", key);
+    string text = get_string("plaintext: ");
+
+    // run
+    for(int i = 0; i < strlen(text); i++)
+    {
+        int preCode = (int) text[i];
+        int newCode = preCode;
+
+        if(preCode >= A && preCode <= Z)
+        {
+            newCode = (preCode - A + key) % 26 + A;
+        }
+        else if(preCode >= a && preCode <= z)
+        {
+            newCode = (preCode - a + key) % 26 + a;
+        }
+
+        
+        char newChar = newCode;
+        text[i] = newChar;
+    }
+
+    printf("ciphertext: %s\n", text);
+ 
+    return false;
+}
+
