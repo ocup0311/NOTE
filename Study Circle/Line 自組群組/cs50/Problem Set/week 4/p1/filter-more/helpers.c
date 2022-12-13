930b794cd2cc5988ee0f12be657fde8467aa26e6
Ocup <ocup0311@gmail.com>
2022-12-13 14:27:13 +0800

    [UPDATE][CS50] week4 p1 filter-more/edges

--- Study Circle/Line 自組群組/cs50/Problem Set/week 4/p1/filter-more/helpers.c
+++ Study Circle/Line 自組群組/cs50/Problem Set/week 4/p1/filter-more/helpers.c
@@ -96,24 +96,10 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
 
 
 // Detect edges
-BYTE cal_GxGy_to_Sz(int (*Gx)[3], int (*Gy)[3])
+BYTE cal_edges(float Sx, float Sy)
 {
-    int Sx = 0, Sy = 0;
-    for(int i = 0; i < 3; i++)
-    {
-        for(int j = 0; j < 3; j++)
-        {
-            Sx += Gx[i][j];
-            Sy += Gy[i][j];
-        }
-    }
+    int result = round(sqrt(pow(Sx, 2) + pow(Sy, 2)));
 
-    // int Sx1 = Sx < 0 ? 0 : Sx > 255 ? 255 : Sx;
-    // int Sy1 = Sy < 0 ? 0 : Sy > 255 ? 255 : Sy;
-
-    int result = round(sqrt(Sx*Sx + Sy*Sy));
-    printf("Sx: %i, Sx^2: %i\n", Sx, Sx*Sx);
-    // printf("Gx: %i ,Sx: %i, Sy: %i, result: %i\n", Gx[0][0], Sx, Sy, result);
     return result > 255 ? 255 : result;
 }
 
@@ -133,58 +119,40 @@ void edges(int height, int width, RGBTRIPLE image[height][width])
     }
 
     // 開始以濾鏡處理圖片
-    int Gx[3][3] = { {-1, 0, 1}, {-2, 0, 2}, {-1, 0, 1} };
-    int Gy[3][3] = { {-1, -2, -1}, {0, 0, 0}, {1, 2, 1} };
-
+    // int Gx[3][3] = { {-1, 0, 1}, {-2, 0, 2}, {-1, 0, 1}};
+    // int Gy[3][3] = { {-1, -2, -1}, {0, 0, 0}, {1, 2, 1}};
+    
     for(int i = 0; i < height; i++)
     {
         for(int j = 0; j < width; j++)
         {
-            int GxR[3][3], GxG[3][3], GxB[3][3], GyR[3][3], GyG[3][3], GyB[3][3];
+            // 以下用來處理一個九宮格
+            // 命名方式： s: start, e: end, h: height, w: width, c: count
+            float SxR = 0, SxG = 0, SxB = 0, SyR = 0, SyG = 0, SyB = 0;
+            int sh = i - 1 > 0 ? i - 1 : 0;
+            int eh = i + 2 < height ? i + 2 : height;
+            int sw = j - 1 > 0 ? j - 1 : 0;
+            int ew = j + 2 < width ? j + 2 : width;
+            int c = (eh - sh) * (ew - sw);
 
-            for (int a = 0; a < 3; a++)
+            for(int a = sh; a < eh; a++)
             {
-                int h = a + i - 1;
-                if(h < 0 || h >= width) continue;
-                for (int b = 0; b < 3; b++)
+                for(int b = sw; b < ew; b++)
                 {
-                    GxR[a][b] = 0;
-                    GxG[a][b] = 0;
-                    GxB[a][b] = 0;
-                    GyR[a][b] = 0;
-                    GyG[a][b] = 0;
-                    GyB[a][b] = 0;
-                    for (int c = 0; c < 3; c++)
-                    {
-                        int w = c + j - 1;
-                        if(w < 0 || w >= height) continue;
-                        // printf("GxR[a][b]: %i,copyImage[h][w].rgbtRed: %i,Gx[c][b]: %i\n",GxR[a][b], copyImage[h][w].rgbtRed, Gx[c][b]);
-                        GxR[a][b] += copyImage[h][w].rgbtRed * Gx[c][b];
-                        GxG[a][b] += copyImage[h][w].rgbtGreen * Gx[c][b];
-                        GxB[a][b] += copyImage[h][w].rgbtBlue * Gy[c][b];
-                        GyR[a][b] += copyImage[h][w].rgbtRed * Gy[c][b];
-                        GyG[a][b] += copyImage[h][w].rgbtGreen * Gy[c][b];
-                        GyB[a][b] += copyImage[h][w].rgbtBlue * Gy[c][b];
-                    }
-                }
+                    int Gx = (b - j) * (a == i ? 2 : 1);
+                    int Gy = (a - i) * (b == j ? 2 : 1);
+                    SxR = SxR + copyImage[a][b].rgbtRed * Gx;
+                    SxG = SxG + copyImage[a][b].rgbtGreen * Gx;
+                    SxB = SxB + copyImage[a][b].rgbtBlue * Gx;
+                    SyR = SyR + copyImage[a][b].rgbtRed * Gy;
+                    SyG = SyG + copyImage[a][b].rgbtGreen * Gy;
+                    SyB = SyB + copyImage[a][b].rgbtBlue * Gy;
+                } 
             }
 
-            // for(int i1 = 0; i1 < 3; i1++)
-            // {
-            //     for(int j1 = 0; j1 < 3; j1++)
-            //     {
-            //         printf("%i, ", GxR[i1][j1]);
-            //     }
-            //     printf("    ");
-            //     for(int j2 = 0; j2 < 3; j2++)
-            //     {
-            //         printf("%i, ", GyR[i1][j2]);
-            //     }
-            //     printf("\n");
-            // }
-            image[i][j].rgbtRed = cal_GxGy_to_Sz(GxR, GyR);
-            image[i][j].rgbtGreen = cal_GxGy_to_Sz(GxG, GyG);
-            image[i][j].rgbtBlue = cal_GxGy_to_Sz(GxB, GyB);
+            image[i][j].rgbtRed = cal_edges(SxR, SyR);
+            image[i][j].rgbtGreen = cal_edges(SxG, SyG);
+            image[i][j].rgbtBlue = cal_edges(SxB, SyB);
         }
     }
 
