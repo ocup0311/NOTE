a5300707b618589371aacb77330406b3303f3766
Ocup <ocup0311@gmail.com>
2023-06-09 01:08:57 +0800

    [ADD][DEVOPS][K8S] kubeadm 安裝

--- DevOps/Kubernetes/note/note.md
+++ DevOps/Kubernetes/note/note.md
@@ -6,6 +6,7 @@
 [Play with Kubernetes]: https://labs.play-with-k8s.com/
 [kubeadm 官方]: https://kubernetes.io/docs/setup/production-environment/tools/kubeadm/create-cluster-kubeadm/
 [Kubernetes Best practices (官方)]: https://kubernetes.io/docs/setup/best-practices/
+[apt-key is deprecated]: https://itsfoss.com/apt-key-deprecated/
 
 <!------------ ref end ------------>
 
@@ -67,11 +68,75 @@
 
   - [kubeadm 官方]
 
-  - Ports & Protocols
+    - 基於 Debian 和 Red Hat
+    - 至少 2GB RAM
+    - 至少 CPU 2 核心
+    - Node 限制：
 
-    ![](../src/image/K8s_Ports_Protocols.png)
+      - 需能互連
+      - 不能有重複的 Host name、MAC address、product_uuid （尤其是 VM 需要檢查）
 
-  -
+        - `/sys/class/dmi/id/product_uuid`
+
+      - 開啟所需 port
+
+        - 用 `netcat` 檢查，EX. `nc 127.0.0.1 6443`
+
+      - 禁用 SWAP
+
+    - 安裝 `CRI- container runtime`、`kubeadm`、`kubelet`、`kubectl`
+
+      - 注意版本相容
+
+  - `install.sh` 步驟
+
+    - 關閉 SWAP
+
+      ![](../src/image/GPT_K8s_SWAP_off.png)
+
+    - 設定 Ports
+
+      - 所有默認端口都可以重新配置
+
+        ![](../src/image/K8s_Ports_Protocols_control.png)
+        ![](../src/image/K8s_Ports_Protocols_worker.png)
+
+      - `iptables` 設定防火牆，以較早設定的為主
+
+        ```sh
+        # EX. 先設定 ACCEPT 再設定成 DROP，則為 ACCEPT
+        $ sudo iptables -A INPUT -p tcp --dport 22 -j ACCEPT
+        $ sudo iptables -P INPUT DROP
+        $ sudo iptables -A INPUT -p tcp --dport 22 -j DROP
+        $ sudo iptables -L INPUT
+        Chain INPUT (policy DROP)
+        target     prot opt source               destination
+        ACCEPT     tcp  --  anywhere             anywhere             tcp dpt:ssh
+        DROP       tcp  --  anywhere             anywhere             tcp dpt:ssh
+
+        # EX. 再將 ACCEPT 刪除後，則為 DROP
+        $ sudo iptables -D INPUT 1
+        $ client_loop: send disconnect: Broken pipe
+        ```
+
+    - 設定所需 Kernel modules
+
+      - 新增在 `/etc/modules-load.d/containerd.conf`
+      - `overlay`：提供容器以 overlay 掛載文件系統，從而實現容器之間的隔離和效率
+      - `br_netfilter`：用於在容器網絡之間進行橋接和網絡封包過濾
+
+    - 設定 Kernel 網路參數
+
+      - 新增在 `/etc/sysctl.d/kubernetes.conf`
+      - 設定可以使用 iptables 跟 ip forward
+
+    - 安裝 apt repo
+
+      - 有兩種方式設定 apt key，但 [apt-key is deprecated]，建議都改成自己管理
+
+    - 安裝 containerd
+
+    - 安裝 kubeadm、kubelet、kubectl
 
 ---
 
--- DevOps/Kubernetes/src/code/install.sh
+++ DevOps/Kubernetes/src/code/install.sh
@@ -4,8 +4,12 @@ echo "[TASK 1] Disable and turn off SWAP"
 sed -i '/swap/d' /etc/fstab
 swapoff -a
 
-echo "[TASK 2] Stop and Disable firewall"
-systemctl disable --now ufw >/dev/null 2>&1
+# echo "[TASK 2] Stop and Disable firewall"
+# systemctl disable --now ufw >/dev/null 2>&1
+echo "[TASK 2] Set firewall"
+sudo iptables -A INPUT -p tcp -m multiport --dports 6443,2379,2380,10250,10259,10257 -j ACCEPT
+sudo iptables -A INPUT -p tcp --match multiport --dports 30000:32767 -j ACCEPT
+
 
 echo "[TASK 3] Enable and Load Kernel modules"
 cat >>/etc/modules-load.d/containerd.conf<<EOF
@@ -28,15 +32,20 @@ mkdir -p /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 echo   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-apt -qq update >/dev/null 2>&1
-apt install -qq -y containerd.io >/dev/null 2>&1
+
+sudo apt -qq update >/dev/null 2>&1
+sudo apt install -qq -y containerd.io >/dev/null 2>&1
 containerd config default >/etc/containerd/config.toml
 systemctl restart containerd
 systemctl enable containerd >/dev/null 2>&1
 
 echo "[TASK 6] Add apt repo for kubernetes"
-curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add - >/dev/null 2>&1
-apt-add-repository "deb http://apt.kubernetes.io/ kubernetes-xenial main" >/dev/null 2>&1
+curl -fsSL https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-archive-keyring.gpg
+echo "deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+# 另一個方式： apt-key deprecated
+# curl -fsSL https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add - 
+# apt-add-repository "deb http://apt.kubernetes.io/ kubernetes-xenial main" >/dev/null 2>&1
 
 echo "[TASK 7] Install Kubernetes components (kubeadm, kubelet and kubectl)"
-apt install -qq -y kubeadm=1.27.2-00 kubelet=1.27.2-00 kubectl=1.27.2-00 >/dev/null 2>&1
+sudo apt install -qq -y kubeadm=1.27.0-00 kubelet=1.27.0-00 kubectl=1.27.0-00 >/dev/null 2>&1
+sudo apt-mark hold kubelet kubeadm kubectl
