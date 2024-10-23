fac8410fd363373f3b45fa55f90b24f8eb7d9576
Ocup <ocup0311@gmail.com>
2024-10-23 23:15:08 +0800

    [UPDATE][MySQL] Leetcode 1148 (DISTINCT vs GROUP BY)
    1. 補充測試 DISTINCT vs GROUP BY 的分析數據

--- DataBase/MySQL/src/code/sample02/distinct_vs_groupby/table.sql
+++ DataBase/MySQL/src/code/sample02/distinct_vs_groupby/table.sql
@@ -1,14 +1,10 @@
 CREATE TABLE IF NOT EXISTS Table1( 
-	id INT NOT NULL AUTO_INCREMENT,
-	k2 VARCHAR(20), 
+	k1 VARCHAR(20),
+	k2 INT,
 	k3 INT, 
 	k4 INT, 
 	k5 INT, 
-	k6 INT, 
-	k7 INT, 
-  PRIMARY KEY (id)
-); 
+	k6 INT
+);
 
-CREATE INDEX idx_k3_k4 ON Table1(k3, k4);
-
-
+CREATE INDEX idx_k2_k3 ON Table1(k2, k3);
